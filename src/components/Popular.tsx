ca325c8666671602153425bc3f24968b677898d1
NatnaelG <natigirma02@gmail.com>
2024-09-26 13:38:31 +0300

    popular pizzas done

--- src/components/Popular.tsx
+++ src/components/Popular.tsx
@@ -1,3 +1,38 @@
-export default function Popular () {
-    return <>Popular</>
+import { Typography } from "@mui/material";
+// import Grid from "@mui/materia/Grid2";
+import Grid from "@mui/material/Grid2";
+import PizzaCard from "./PizzaCard";
+
+export default function Popular() {
+  return (
+    <>
+      <Typography sx={{ color: "text.secondary", fontSize: "50px" }}>
+        Popular Pizzas
+      </Typography>
+
+      <Grid container spacing={3}>
+        <Grid size={4}>
+          <PizzaCard />
+        </Grid>
+        <Grid size={4}>
+          <PizzaCard />
+        </Grid>
+        <Grid size={4}>
+          <PizzaCard />
+        </Grid>
+        <Grid size={4}>
+          <PizzaCard />
+        </Grid>
+        <Grid size={4}>
+          <PizzaCard />
+        </Grid>
+        <Grid size={4}>
+          <PizzaCard />
+        </Grid>
+        <Grid size={4}>
+          <PizzaCard />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
