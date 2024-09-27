46a9b16affbac0da5d3de9ca67a10bf6faa65234
NatnaelG <natigirma02@gmail.com>
2024-09-27 03:24:49 +0300

    Hero component started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Fasting from "@/components/Fasting";
 import FeaturedPizzaWrapper from "@/components/FeaturedPizzaWrapper";
 import BottomNav from "@/components/footer/BottomNav";
 import Footer from "@/components/footer/footer";
+import Hero from "@/components/Hero";
 // import OrderHistoryPizza from "@/components/OrderHistoryPizza";
 // import PizzaCard from "@/components/PizzaCard";
 // import RelatedPizza from "@/components/RelatedPizza";
@@ -12,6 +13,9 @@ import { Box } from "@mui/material";
 export default function Home() {
   return (
     <>
+      <Box p={3}>
+        <Hero />
+      </Box>
       <Box p={3}>
         <FeaturedPizzaWrapper />
       </Box>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,50 @@
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+  Stack,
+  Typography,
+} from "@mui/material";
+
+export default function Hero() {
+  return (
+    <>
+      <Stack width={"766px"} height={"333px"}>
+        <Typography
+          fontWeight={700}
+          fontSize={"150px"}
+          color="linear-gradient(to right, #FF8100, #FFBE71)"
+        >
+          Order us
+        </Typography>
+        <Typography fontWeight={400} fontSize={"25"} color="">
+          In publishing and graphic design, Lorem ipsum is a placeholder text
+          commonly used to demonstrate the visual form of a document or a
+          typeface without.
+        </Typography>
+        <OutlinedInput
+            endAdornment={
+              <InputAdornment position="end" sx={{ pr: 2 }}>
+                <IconButton
+                  sx={{ color: "#FF8100" }}
+                  aria-label="send"
+                  edge="end"
+                  size="large"
+                >
+                  <SendIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+            placeholder="Your feedback..."
+            sx={{
+              background: "#fff",
+              width: "748px",
+              px: 1,
+              borderRadius: "15px",
+            }}
+          />
+      </Stack>
+    </>
+  );
+}
