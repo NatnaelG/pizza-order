1cce2123fb3b823fd54951b5089e3e7b37b28a79
NatnaelG <natigirma02@gmail.com>
2024-09-30 17:26:01 +0300

    issue fixed

--- src/components/role/RoleModal.tsx
+++ src/components/role/RoleModal.tsx
@@ -60,7 +60,7 @@ export default function RoleModal({
   const [permissions, setPermissions] = React.useState<string[]>(
     roleDialog.role?.permissions || []
   );
-  console.log("to use setPermissions", roleDialog.role?.permissions,permissions);
+  console.log("to use setPermissions", setPermissions, roleDialog.role?.permissions,permissions);
   React.useEffect(() => {
     if (state?.message === "success") {
       handleClose();
