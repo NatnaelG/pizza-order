bd902d15de0164a69e529631231a294041fd96b2
NatnaelG <natigirma02@gmail.com>
2024-10-01 07:26:54 +0300

    build error fixed

--- src/components/add-menu/SuccessModal.tsx
+++ src/components/add-menu/SuccessModal.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import { IconButton, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import CheckIcon from "@mui/icons-material/Check";
 
 const style = {
