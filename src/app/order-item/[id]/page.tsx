560da24efc8bca15f158ce81d79bc34bb59e88f4
NatnaelG <natigirma02@gmail.com>
2024-10-02 11:52:45 +0300

    ui modification

--- src/app/order-item/[id]/page.tsx
+++ src/app/order-item/[id]/page.tsx
@@ -7,13 +7,11 @@ import Grid from "@mui/material/Grid2";
 import { getMenu } from "@/lib/order-item/order-item-management";
 
 export default async function MenuItem({ params }: { params: { id: string } }) {
-
-const menu = await getMenu(params.id);
-
+  const menu = await getMenu(params.id);
 
   return (
     <>
-      <Box p={3}>
+      <Box p={3} width={"100%"}>
         <Grid container spacing={2}>
           <Grid
             //  key={permission + " " + index}
@@ -23,14 +21,15 @@ const menu = await getMenu(params.id);
           <Grid
             //  key={permission + " " + index}
             size={{ xs: 12, lg: 6 }}
-            sx={{maxWidth: {xs: "600px", lg: "inherit"}}}
+            sx={{ maxWidth: { xs: "600px", lg: "inherit" } }}
           >
             <OrderItem menu={typeof menu === "string" ? null : menu} />
           </Grid>
         </Grid>
       </Box>
-
-      <Related />
+      <Box p={3} width={"100%"}>
+        <Related />
+      </Box>
     </>
   );
 }
