a5af3835e2692ccfacc871c68b0fb59907bca22b
NatnaelG <natigirma02@gmail.com>
2024-11-14 14:21:22 +0300

    access contorl feture for non logged in user test done

--- tests/features/access_control.feature
+++ tests/features/access_control.feature
@@ -1,8 +1,15 @@
 @web @accessContorl
 Feature: Access Control
 
-    Scenario: A non logged user attempts to order
+    @clickOrderButton
+    Scenario: A non logged user attempts to order by clicking order button
 
         Given A non logged in user is in the homepage
         When The user attempts to order a menu
-        Then The user should be redirected to login page
+        Then The user should be redirected to "login" page
+
+    @urlQuery
+    Scenario: A non logged user attempts order item page using query
+
+        Given A non logged in user enters "https://pizza-order-sepia.vercel.app/order-item/"
+        Then The user should be redirected to "login" page
--- tests/features/step_definitions/access_control_steps.ts
+++ tests/features/step_definitions/access_control_steps.ts
@@ -7,13 +7,18 @@ Given("A non logged in user is in the homepage", async function () {
   await this.page.goto(`https://pizza-order-sepia.vercel.app/`);
 });
 
-When(
-  "The user attempts to order a menu",
-  async function () {
-    await this.page.locator(`button[data-id="order5a2439a1-a22e-4cd5-8f22-47b3a5057a22"]`).click();
-  }
-);
+Given("A non logged in user enters {string}", async function (path) {
+  await this.page.goto(`${path}`);
+});
 
-Then("The user should be redirected to login page", async function () {
-  await this.page.waitForURL("https://pizza-order-sepia.vercel.app/");
+When("The user attempts to order a menu", async function () {
+  await this.page
+    .locator(
+      `button[data-id="order 5a2439a1-a22e-4cd5-8f22-47b3a5057a22 Popular"]`
+    )
+    .click();
+});
+
+Then("The user should be redirected to {string} page", async function (path) {
+  await this.page.waitForURL(`https://pizza-order-sepia.vercel.app/${path}`);
 });
