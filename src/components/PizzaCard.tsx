80056d9fae12e850d1628a0d138c9350944920c5
NatnaelG <natigirma02@gmail.com>
2024-11-14 13:40:55 +0300

    added menu id in the data id

--- src/components/PizzaCard.tsx
+++ src/components/PizzaCard.tsx
@@ -86,7 +86,7 @@ export default function PizzaCard({
                   background: "#FF8100",
                 }}
                 variant={"contained"}
-                data-id={"order"}
+                data-id={`order${menu.id}`}
               >
                 Order
               </Button>
