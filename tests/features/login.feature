b52136bd2307886423f08106274f9ff1da8c3d9e
NatnaelG <natigirma02@gmail.com>
2024-11-14 09:39:10 +0300

    modified the login test feature and steps

--- tests/features/login.feature
+++ tests/features/login.feature
@@ -4,6 +4,7 @@ Feature: Login
     @web
     Scenario: A user attempts login with correct credentails
 
-        Given A user is on the login page
+        Given A user is on the "login" page
         When A user logs in with valid credentails
         Then The user should be redirected to homepage
+        And "LOGOUT" button should be there
--- tests/features/step_definitions/login_steps.ts
+++ tests/features/step_definitions/login_steps.ts
@@ -3,8 +3,9 @@ import { expect } from "@playwright/test";
 
 setDefaultTimeout(60 * 1000);
 
-Given("A user is on the login page", async function () {
-  await this.page.goto(`https://pizza-order-sepia.vercel.app/login`);
+Given("A user is on the {string} page", async function (path) {
+  await this.page.goto(`https://pizza-order-sepia.vercel.app/${path}`);
+  // await this.page.goto(`/login`);
   // await this.page.goto(`http://localhost:3000/login`);
 });
 
@@ -13,16 +14,20 @@ When("A user logs in with valid credentails", async function () {
   await this.page.fill("input#password", "123456");
 
   await this.page.locator("button#Login").click();
-  await this.page.waitForURL("https://pizza-order-sepia.vercel.app/");
 });
 
 Then("The user should be redirected to homepage", async function () {
+  await this.page.waitForURL("https://pizza-order-sepia.vercel.app/");
+});
+
+Then("{string} button should be there", async function (buttonText) {
   // const locator = await this.page.locator(`p#LogOut`);
   const locator = await this.page.locator(`text=LogOut`);
   const text = await locator.innerText();
 
   // const count = await locator.count();
   // expect(count).toBeGreaterThan(0);
+
   // await expect(locator).toHaveText("LogOut");
-  expect(text).toBe("LOGOUT");
+  expect(text).toBe(buttonText);
 });
